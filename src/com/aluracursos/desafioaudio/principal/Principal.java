7e787d0b7b63dacebb1ecd2bb452970b5f084eec
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-05-20 18:58:34 -0600

    2024-05-20 Creando métodos reutilizables

--- src/com/aluracursos/desafioaudio/principal/Principal.java
+++ src/com/aluracursos/desafioaudio/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.aluracursos.desafioaudio.principal;
 
 import com.aluracursos.desafioaudio.modelo.Cancion;
+import com.aluracursos.desafioaudio.modelo.MisFavoritos;
 import com.aluracursos.desafioaudio.modelo.Podcast;
 
 public class Principal {
@@ -14,15 +15,29 @@ public class Principal {
         miPodcast.setPresentador("Gabriela Aguiar");
         miPodcast.setTitulo("Café Tech");
 
+        // CANCIÓN
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
 
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 2000; i++) {
             miCancion.reproducir();
+        }
+
+        // PODCAST
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproducir();
         }
 
         System.out.println("Total de Reproducciones: " + miCancion.getTotalReproducciones());
         System.out.println("Total de Me Gusta: " + miCancion.getMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(miCancion);
+        favoritos.adicione(miPodcast);
     }
 }
